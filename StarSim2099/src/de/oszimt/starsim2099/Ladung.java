e69fee1ee1baba4e7a256cc3dd6d47c22a59d075
Simon <simon.eckardt.schule@gmail.com>
2026-09-17 10:33:24 +0200

    Implemented Ladung.java

--- StarSim2099/src/de/oszimt/starsim2099/Ladung.java
+++ StarSim2099/src/de/oszimt/starsim2099/Ladung.java
@@ -9,12 +9,47 @@ package de.oszimt.starsim2099;
 public class Ladung {
 
 	// Attribute
-
+	private String typ;
+	private int masee;
+	private double posX;
+	private double posY;
 	// Methoden
+	public String getTyp() {
+		return typ;
+	}
 
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+
+	public int getMasee() {
+		return masee;
+	}
+
+	public void setMasee(int masee) {
+		this.masee = masee;
+	}
+
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] ladungShape = { { '/', 'X', '\\' }, { '|', 'X', '|' }, { '\\', 'X', '/' } };
 		return ladungShape;
 	}
-}
+	}
+
+	
