1f541d84a17238f4cde97bc9336856452c4eeabe
Simon <simon.eckardt.schule@gmail.com>
2026-09-24 10:16:35 +0200

    added a new Superclass (also inherits from SuperBum)

---
+++ StarSim2099/src/de/oszimt/starsim2099/Himmelskoerper.java
@@ -0,0 +1,17 @@
+package de.oszimt.starsim2099;
+
+public class Himmelskoerper extends SuperBum{
+	private String typ;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTyp() {
+		return typ;
+	}
+}
