b1e79e4885eb066db2ba9fceb94ebebf7b253c9c
Simon <simon.eckardt.schule@gmail.com>
2026-09-24 10:15:04 +0200

    added a Superclass with PosX and PosY

---
+++ StarSim2099/src/de/oszimt/starsim2099/SuperBum.java
@@ -0,0 +1,23 @@
+package de.oszimt.starsim2099;
+
+public class SuperBum {
+private double posX;
+private double posY;
+
+public double getPosX() {
+	return posX;
+}
+
+public void setPosX(double posX) {
+	this.posX = posX;
+}
+
+public double getPosY() {
+	return posY;
+}
+
+public void setPosY(double posY) {
+	this.posY = posY;
+}
+
+}
